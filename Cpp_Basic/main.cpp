6712ebb50c9e56c4c36691cc2c4c62ff78f1ce5a
Rylah <94848571+rylahs@users.noreply.github.com>
2024-01-28 00:21:38 +0900

    02. 변수

--- Cpp_Basic/main.cpp
+++ Cpp_Basic/main.cpp
@@ -21,6 +21,55 @@ int main(void)
 	std::cout << "가나다라" << std::endl;
 	
 	cout << "std namespace 사용" << endl;
+	/*
+	변수 : 변하는 수. 값이 바뀔 수 있다.
+        용량의 최소 단위 : bit
+        1byte = 8bit
+        1kbyte = 1024byte
+        1mbyte = 1024kbyte
+        1gbyte = 1024mbyte
+        1tbyte = 1024gbyte
+
+        문자를 표현하는 방법 ASCI 코드 표를 가지고 있다.
+        false : 0
+        true : 0이 아닌 모든 수
+
+        unsigned : 부호가 없다.
+        종류   ㅣ  용량  ㅣ  데이터  ㅣ   표현범위       ㅣ  unsigned     ㅣ
+        char   ㅣ 1byte ㅣ   문자   ㅣ  -128 ~ 127     ㅣ  0 ~ 255      ㅣ
+        bool   ㅣ 1byte ㅣ 참, 거짓 ㅣ true / false    ㅣ  true / false ㅣ
+        short  ㅣ 2byte ㅣ   정수   ㅣ -32768 ~ 32767  ㅣ 0 ~ 65535     ㅣ
+        int    ㅣ 4byte ㅣ   정수   ㅣ 약 -21억 ~ 21억  ㅣ 0 ~ 약 43억   ㅣ
+        float  ㅣ 4byte ㅣ   실수   ㅣ
+        double ㅣ 8byte ㅣ   실수   ㅣ
+	*/
+    // = : 대입연산자이다. 오른쪽에 있는 값을 왼쪽으로 대입한다.
+    int number = 10;
+    number = 20;
+
+    cout << number << endl;
+
+    bool    bTest = true;
+    bTest = false;
+
+    cout << bTest << endl;
+
+    // 영문이나 숫자는 1byte를 차지한다.
+    // 하지만 한글이나 한문 등의 문자열은 2byte를 차지하기 떄문에
+    // char 변수 하나에 저장하기가 힘들다.
+    char    cTest = 't';
+
+    cout << cTest << endl;
+
+    float fNumber = 3.14f;
+
+    cout << fNumber << endl;
+
+    double dNumber = 123.4567;
+
+    cout << dNumber << endl;
+
+
 
 	return 0;
 }
