5bc82d2dc4c84a590361363cd6932b8e985ee24f
Rylah <94848571+rylahs@users.noreply.github.com>
2024-01-28 03:53:05 +0900

    hwans

--- Cpp_Basic/hw1ans.cpp
+++ Cpp_Basic/hw1ans.cpp
@@ -10,8 +10,8 @@ void hw1()
 		}
 		cout << endl;
 	}
+	cout << endl;
 }
-
 void hw1_1()
 {
 	/*
@@ -31,28 +31,30 @@ void hw1_1()
 	}
 }
 
-
 void hw2()
 {
 	for (int i = 0; i < 5; ++i)
 	{
-		for (int j = 0; j <= i; ++j)
+		for (int j = 0; j < i + 1; ++j)
 		{
 			cout << "*";
 		}
 		cout << endl;
 	}
+
+	cout << endl;
 }
 void hw3()
 {
 	for (int i = 0; i < 5; ++i)
 	{
-		for (int j = i; j < 5; ++j)
+		for (int j = 0; j < 5 - i; ++j)
 		{
 			cout << "*";
 		}
 		cout << endl;
 	}
+	cout << endl;
 }
 void hw4()
 {
@@ -63,34 +65,50 @@ void hw4()
 			cout << " ";
 		}
 		for (int j = 0; j < 2 * i + 1; ++j)
-		{ 
+		{
 			cout << "*";
 		}
 		cout << endl;
 	}
+	cout << endl;
 }
 
 void hw5()
 {
-	for (int i = 0; i < 4; ++i)
+	/*
+	    *
+	   ***
+	  *****
+	 *******
+	  *****
+	   ***
+	    *
+	*/
+	int iLine = 7; // 별의 최대 개수
+	int iCount = 0;
+	for (int i = 0; i < iLine; ++i)
 	{
-		for (int j = 0; j < 3 - i; ++j)
+		// 공백 : 3 2 1 0 1 2 3
+		// 별 : 1 3 5 7 5 3 1
+		iCount = i;
+
+		// i 값이 7줄 기준 4 5 6 즉 밑의 삼각형을 구성할 때만 if에 들어간다.
+
+		if (i > iLine / 2)
+		{
+			iCount = iLine - 1 - i;
+		}
+
+		// i 값이 0 1 2 3일때에는 iCount는 i값을 그대로 대입한다.
+		// i 갑싱 4 5 6일때는 iCount는 2 1 0이다
+		// 즉 최종 i 값은 0 1 2 3 2 1 0으로 들어가게 된다.
+
+		for (int j = 0; j < 3 - iCount; ++j)
 		{
 			cout << " ";
 		}
-		for (int j = 0; j < 2 * i + 1; ++j)
-		{
-			cout << "*";
-		}
-		cout << endl;
-	}
-	for (int i = 0; i < 3; ++i)
-	{
-		for (int j = 0; j < i + 1; ++j)
-		{
-			cout << " ";
-		}
-		for (int j = 0; j < 2 * (3 - i) - 1; ++j)
+		
+		for (int j = 0; j < iCount * 2 + 1; ++j)
 		{
 			cout << "*";
 		}
