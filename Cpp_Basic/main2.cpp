5374d285b8b8af5d635da1959bbf8b9022b77ffd
Rylah <94848571+rylahs@users.noreply.github.com>
2024-01-28 02:32:53 +0900

    난수와 확률 & if문의 활용

--- Cpp_Basic/main2.cpp
+++ Cpp_Basic/main2.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <ctime>
 using namespace std;
 
 int main(void)
@@ -156,6 +156,69 @@ int main(void)
 		cout << "31 ~ 40 사이의 숫자입니다." << endl;
 	else
 		cout << "그 외의 숫자입니다." << endl;
+
+	// 난수 발생
+	srand((unsigned int)time(NULL));
+
+	cout << rand() << endl;
+	cout << rand() << endl;
+	cout << rand() << endl;
+
+	cout << (rand() % 100) << endl;
+	cout << (rand() % 101) + 100 << endl;
+
+	cout << (rand() % 10000 / 100.0f) << endl;
+	
+	int iUpgrade = 0;
+	cout << "Upgrade 기본 수치를 입력하세요 : ";
+	cin >> iUpgrade;
+
+
+	// 강화 확률을 구한다.
+	float fPercent = rand() % 10000 / 100.0f;
+
+	// 강화 확률 업그레이드가 
+	// 0 ~ 3 : 100% 성공
+	// 4 ~ 6 : 40%
+	// 7 ~ 9 : 10%
+	// 10 ~ 13 : 1%
+	// 14 ~ 15 : 0.01%
+
+	cout << "Upgrade : " << iUpgrade << endl;
+	cout << "Percent : " << fPercent << endl;
+
+	if (iUpgrade <= 3)
+		cout << "강화 성공" << endl;
+
+	else if (4 <= iUpgrade && iUpgrade <= 6)
+	{
+		if (fPercent < 40.0f)
+			cout << "강화 성공" << endl;
+		else
+			cout << "강화 실패" << endl;
+	}
+	else if (7 <= iUpgrade && iUpgrade <= 9)
+	{
+		if (fPercent < 10.0f)
+			cout << "강화 성공" << endl;
+		else
+			cout << "강화 실패" << endl;
+	}
+	else if (10 <= iUpgrade && iUpgrade <= 13)
+	{
+		if (fPercent < 1.0f)
+			cout << "강화 성공" << endl;
+		else
+			cout << "강화 실패" << endl;
+	}
+	else if (14 <= iUpgrade && iUpgrade <= 15)
+	{
+		if (fPercent < 0.1f)
+			cout << "강화 성공" << endl;
+		else
+			cout << "강화 실패" << endl;
+	}
+
 
 	return 0;
 }
