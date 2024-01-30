80270b0c9550c91b1d48a11b4e72993789630e72
Rylah <94848571+rylahs@users.noreply.github.com>
2024-01-30 23:41:33 +0900

    TextRPG1

---
+++ Cpp_Basic/TextRPG.cpp
@@ -0,0 +1,282 @@
+
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+enum MAIN_MENU
+{
+	MM_NONE,
+	MM_MAP,
+	MM_STORE,
+	MM_INVENTORY,
+	MM_EXIT
+};
+
+enum MAP_TYPE
+{
+	MT_NONE,
+	MT_EASY,
+	MT_NORMAL,
+	MT_HARD,
+	MT_BACK
+
+};
+
+enum JOB
+{
+	JOB_NONE,
+	JOB_KNIGHT,
+	JOB_ARCHER,
+	JOB_WIZARD,
+	JOB_END
+};
+
+#define NAME_SIZE	32
+
+struct _tagPlayer
+{
+	char	strName[NAME_SIZE];
+	char	strJobName[NAME_SIZE];
+	JOB		eJob;
+	int		iAttackMin;
+	int		iAttackMax;
+	int		iArmorMin;
+	int		iArmorMax;
+	int		iHp;
+	int		iHpMax;
+	int		iMp;
+	int		iMpMax;
+	int		iExp;
+	int		iLevel;
+};
+
+struct _tagMonster
+{
+	char	strName[NAME_SIZE];
+	int		iAttackMin;
+	int		iAttackMax;
+	int		iArmorMin;
+	int		iArmorMax;
+	int		iHp;
+	int		iHpMax;
+	int		iMp;
+	int		iMpMax;
+	int		iLevel;
+	int		iExp;
+	int		iGoldMin;
+	int		iGoldMax;
+};
+
+int main(void)
+{
+	srand((unsigned int)time(NULL));
+
+	// 게임 시작할 떄 플레이어 정보를 설정하게 된다.
+	_tagPlayer	tPlayer = {};
+
+	// 플레이어 이름을 입력 받는다.
+	cout << "이름 : ";
+	cin.getline(tPlayer.strName, NAME_SIZE - 1);
+	int iJob = JOB_NONE;
+	while (iJob == JOB_NONE)
+	{
+		system("cls");
+		cout << "1. 기사" << endl;
+		cout << "2. 궁수" << endl;
+		cout << "3. 마법사" << endl;
+		cout << "직업을 선택하세요 : ";
+		cin >> iJob;
+
+		if (cin.fail())
+		{
+			cin.clear();
+			cin.ignore(1024, '\n');
+			continue;
+		}
+
+		else if (iJob <= JOB_NONE || iJob >= JOB_END)
+			iJob = JOB_NONE;
+
+	}
+
+	tPlayer.iLevel = 1;
+	tPlayer.iExp = 0;
+	tPlayer.eJob = (JOB)iJob;
+
+	switch (tPlayer.eJob)
+	{
+	case JOB_KNIGHT:
+		strcpy_s(tPlayer.strJobName, "기사");
+		tPlayer.iAttackMin = 5;
+		tPlayer.iAttackMax = 10;
+		tPlayer.iArmorMin = 15;
+		tPlayer.iArmorMax = 20;
+		tPlayer.iHpMax = 500;
+		tPlayer.iHp = 500;
+		tPlayer.iMp = 100;
+		tPlayer.iMpMax = 100;
+
+		break;
+	case JOB_ARCHER:
+		strcpy_s(tPlayer.strJobName, "궁수");
+		tPlayer.iAttackMin = 10;
+		tPlayer.iAttackMax = 15;
+		tPlayer.iArmorMin = 10;
+		tPlayer.iArmorMax = 15;
+		tPlayer.iHpMax = 400;
+		tPlayer.iHp = 400;
+		tPlayer.iMp = 200;
+		tPlayer.iMpMax = 200;
+
+		break;
+	case JOB_WIZARD:
+		strcpy_s(tPlayer.strJobName, "마법사");
+		tPlayer.iAttackMin = 15;
+		tPlayer.iAttackMax = 20;
+		tPlayer.iArmorMin = 5;
+		tPlayer.iArmorMax = 10;
+		tPlayer.iHpMax = 300;
+		tPlayer.iHp = 300;
+		tPlayer.iMp = 300;
+		tPlayer.iMpMax = 300;
+
+		break;
+	}
+
+	// 몬스터를 생성한다.
+	_tagMonster tMonsterArr[MT_BACK - 1] = {};
+
+	// 고블린 생성
+	strcpy_s(tMonsterArr[0].strName, "고블린");
+	tMonsterArr[0].iAttackMin = 20;
+	tMonsterArr[0].iAttackMax = 30;
+	tMonsterArr[0].iArmorMin = 2;
+	tMonsterArr[0].iArmorMax = 5;
+	tMonsterArr[0].iHp = 100;
+	tMonsterArr[0].iHpMax = 100;
+	tMonsterArr[0].iMp = 10;
+	tMonsterArr[0].iMpMax = 10;
+	tMonsterArr[0].iLevel = 1;
+	tMonsterArr[0].iExp = 1000;
+	tMonsterArr[0].iGoldMin = 500;
+	tMonsterArr[0].iGoldMax = 1500;
+
+	// 트롤 생성
+	strcpy_s(tMonsterArr[1].strName, "트롤");
+	tMonsterArr[1].iAttackMin = 80;
+	tMonsterArr[1].iAttackMax = 130;
+	tMonsterArr[1].iArmorMin = 60;
+	tMonsterArr[1].iArmorMax = 90;
+	tMonsterArr[1].iHp = 2000;
+	tMonsterArr[1].iHpMax = 2000;
+	tMonsterArr[1].iMp = 100;
+	tMonsterArr[1].iMpMax = 100;
+	tMonsterArr[1].iLevel = 5;
+	tMonsterArr[1].iExp = 7000;
+	tMonsterArr[1].iGoldMin = 6000;
+	tMonsterArr[1].iGoldMax = 8000;
+
+	// 드래곤 생성
+	strcpy_s(tMonsterArr[2].strName, "드래곤");
+	tMonsterArr[2].iAttackMin = 250;
+	tMonsterArr[2].iAttackMax = 500;
+	tMonsterArr[2].iArmorMin = 200;
+	tMonsterArr[2].iArmorMax = 400;
+	tMonsterArr[2].iHp = 30000;
+	tMonsterArr[2].iHpMax = 30000;
+	tMonsterArr[2].iMp = 20000;
+	tMonsterArr[2].iMpMax = 20000;
+	tMonsterArr[2].iLevel = 10;
+	tMonsterArr[2].iExp = 30000;
+	tMonsterArr[2].iGoldMin = 20000;
+	tMonsterArr[2].iGoldMax = 50000;
+
+
+	while (true)
+	{
+		system("cls");
+		cout << "==================== 로비 ====================" << endl;
+		cout << "1. 맵" << endl;
+		cout << "2. 상점" << endl;
+		cout << "3. 가방" << endl;
+		cout << "4. 종료" << endl;
+		cout << "메뉴를 선택하세요 : ";
+
+		int iMenu;
+		cin >> iMenu;
+
+		if (cin.fail())
+		{
+			cin.clear();
+			cin.ignore(1024, '\n');
+			continue;
+		}
+
+		if (iMenu == MM_EXIT)
+			break;
+
+		switch (iMenu)
+		{
+		case MM_MAP:
+			while (true)
+			{
+				system("cls");
+				cout << "==================== 맵 ====================" << endl;
+				cout << "1. 쉬움" << endl;
+				cout << "2. 보통" << endl;
+				cout << "3. 어려움" << endl;
+				cout << "4. 뒤로가기" << endl;
+				cout << "맵을 선택 하세요 : ";
+				cin >> iMenu;
+
+				if (cin.fail())
+				{
+					cin.clear();
+					cin.ignore(1024, '\n');
+					continue;
+				}
+
+				// 이 if문에 속한 break는 맵 메뉴를 돌려주기 위한 while에 속해있으므로 
+				// 이 while문을 빠져나간다.
+				if (iMenu == MT_BACK)
+					break;
+
+				// 선택한 메뉴에서 1을 뺴주면 몬스터 배열의 인덱스가 된다.
+				// 그렇게 해서 해당 맵의 몬스터를 생성해준다.
+
+				_tagMonster tMonster = tMonsterArr[iMenu - 1];
+
+				while (true)
+				{
+					switch (iMenu)
+					{
+					case MT_EASY:
+						cout << "==================== 쉬움 ====================" << endl;
+						break;
+					case MT_NORMAL:
+						cout << "==================== 보통 ====================" << endl;
+						break;
+					case MT_HARD:
+						cout << "==================== 어려움 ====================" << endl;
+						break;
+					}
+
+					
+				}
+
+			}
+			break;
+		case MM_STORE:
+			break;
+		case MM_INVENTORY:
+			break;
+		default:
+			break;
+		}
+
+
+	}
+
+	return 0;
+}
